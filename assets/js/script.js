efefd817855274024a3b6dcbc14eccd9bd447997
Lady Trell <antrellkent@gmail.com>
2022-03-10 19:04:06 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var tasks = {};
 
 var auditTask = function(taskEl) {
+  console.log(taskEl)
   // get date from task element
   var date = $(taskEl).find("span").text().trim();
  
@@ -160,7 +161,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -196,17 +197,26 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
     console.log("activate", this);
   },
   deactivate: function(event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
     console.log("deactivate", this);
   },
-  over: function(event) {
+  over: function(event) { 
+    $(this).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-activate");
     console.log("over", event.target);
   },
   out: function(event) {
+    $(this).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-activate");
     console.log("out", event.target);
   },
+
   update: function(event) {
     // array to store the task data in
     var tempArr = [];
@@ -264,4 +274,8 @@ $("#modalDueDate").datepicker({
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
